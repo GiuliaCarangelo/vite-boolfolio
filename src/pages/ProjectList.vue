6a4595c5967113a43e6c13c9a21173eb87758a0b
GiuliaCarangelo <tnhkagura@gmail.com>
2024-02-26 17:43:12 +0100

    import axios

---
+++ src/pages/ProjectList.vue
@@ -0,0 +1,25 @@
+<script>
+import axios from 'axios';
+
+export default {
+    name: 'ProjectList',
+    data(){
+        return{
+            projects: [],
+        }
+    },
+    methods:{
+        getProjects(){
+            axios.get('http://127.0.0.1:8000/api/projects', {
+                params: {
+                }
+            })
+            .then((response) => {
+                console.log(response.data.results.data);
+                this.projects = response.data.results.data;
+
+            })
+        }
+    }
+}
+</script>
